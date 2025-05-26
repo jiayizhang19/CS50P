6c4ca2d9ce42b46334ad8f1d3a731b96c2aa037c
j.zhang19 <j.zhang19@outlook.com>
2025-05-26 20:35:14 +0800

    update 5 unit tests notes

--- 5-UnitTests/5.1-Pytest/test_hello.py
+++ 5-UnitTests/5.1-Pytest/test_hello.py
@@ -2,7 +2,9 @@
 Note: 
 Assert statement we are using here, is designed to test arguments into functions and return values they're from. Not testing side effects.
 
-
+For example, if hello function is written as below, there will be failures to test it, as it only has side effect, instead of a return value.
+def hello(to="world"):
+    print("hello,", to)
 
 """
 
