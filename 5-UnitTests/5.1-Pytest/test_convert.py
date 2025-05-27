f6b60fe1665c1b925fa9624ffcf8ef1c0f4a54f8
j.zhang19 <j.zhang19@outlook.com>
2025-05-27 19:18:09 +0800

    update lecture notes

--- 5-UnitTests/5.1-Pytest/test_convert.py
+++ 5-UnitTests/5.1-Pytest/test_convert.py
@@ -1,5 +1,5 @@
 """
-We have to use pytest.raises() to check exceptions, instead of using assert 
+Note: We have to use pytest.raises() to check exceptions, instead of assert 
 
 """
 
@@ -10,9 +10,14 @@ def test_integer():
     assert convert(1) == 149597870700
 
 
-def test_float():
+def test_float_without_toletance():
     assert convert(3.3) == 3.3 * 149597870700
 
+
+def test_float_with_tolerance():
+    assert convert(0.001) == pytest.approx(149597870.691)
+    assert convert(0.001) == pytest.approx(149597870.691, abs=1e-5)
+    
 
 def test_string():
     with pytest.raises(TypeError):
@@ -20,6 +25,6 @@ def test_string():
 
 
 def test_string_format_number():
-    # assert convert("1") == "TypeError: au must be an integer or float"
+    # assert convert("1") == "TypeError: au must be an integer or float" --> Incorrect way to check exception!
     with pytest.raises(TypeError):
         convert("1")
