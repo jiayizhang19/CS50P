12b56ce8b5137ab75720fa5b03f8e920f19d7a9f
jiayizhang19 <79315650+jiayizhang19@users.noreply.github.com>
2025-06-19 04:01:58 +0000

    update shorts notes

--- 8-ObjectOrientedProgramming/8.2-Shorts/food.py
+++ 8-ObjectOrientedProgramming/8.2-Shorts/food.py
@@ -18,17 +18,18 @@ class Food:
     # define a function to return the instance of the class without specifying ingredients but hearts
     @classmethod
     def from_nothing(cls, hearts):
-        food = Food([])
+        food = Food([]) # or it could be written as food = cls([])
+        # after instantiating, overwrite the hearts with provided ones
         food.hearts = cls.base_hearts + hearts
         return food
 
 
 def main():
     mushroom_skewer = Food(["Mushroom", "Hearty Mushroom"])
-    print(f"This skewer heals {mushroom_skewer.hearts} hearts")
+    print(f"{mushroom_skewer.ingredients} heals {mushroom_skewer.hearts} hearts ")
 
     non_ingredients_skewer = Food.from_nothing(2)
-    print(f"This skewer heals {non_ingredients_skewer.hearts} hearts")
+    print(f"{non_ingredients_skewer.ingredients} heals {non_ingredients_skewer.hearts} hearts")
 
 
 if __name__ == "__main__":
