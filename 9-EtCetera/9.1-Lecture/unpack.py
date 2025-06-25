6e978ef232fa4c65e35f80f9fcaa4df37a5731a7
jiayizhang19 <j.zhang19@outlook.com>
2025-06-25 12:03:36 +0800

    update notes

--- 9-EtCetera/9.1-Lecture/unpack.py
+++ 9-EtCetera/9.1-Lecture/unpack.py
@@ -1,5 +1,5 @@
 """
-Using the * to unpack a list / tuple into positional arguments.
+Using the * to unpack a list / tuple into positional arguments. It can be used with any iterable types.
     *coins == coins[0], coins[1], coins[2]
 Using the ** to unpack a dictionary into keyword arguments.
     **coins == coins['galleons'], coins['sickles'], coins['knuts']
@@ -9,15 +9,15 @@ Using the **kwargs to accept a variable number of keyword arguments in a functio
 """
 
 # ============= Function to unpack list and disctionary =============
-# def total(galleons, sickles, knuts):
-#     return (galleons * 17 + sickles) * 29 + knuts
+def total(galleons, sickles, knuts):
+    return (galleons * 17 + sickles) * 29 + knuts
 
 
-# coins = [100, 50, 25]
-# print(total(*coins), "Knuts")
+coins = [100, 50, 25]
+print(total(*coins), "Knuts")
 
-# coins_dict = {'galleons': 100, 'sickles': 50, 'knuts': 25}
-# print(total(**coins_dict), "Knuts")
+coins_dict = {'galleons': 100, 'sickles': 50, 'knuts': 25}
+print(total(**coins_dict), "Knuts")
 
 # ============= Function with *args and **kwargs =============
 def f(*args, **kwargs):
