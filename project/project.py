5a2be863a25c5237cc4c9b5409aaeadc3b31ad3d
jiayizhang19 <j.zhang19@outlook.com>
2025-06-30 13:46:04 +0800

    complete record_speech

--- project/project.py
+++ project/project.py
@@ -4,28 +4,32 @@ def main():
     ...
 
 
-def record_speech():
-    audio = get_audio()
+def record_speech(audio):
     with open("recorded.wav", "wb") as f:
-            f.write(audio.get_wav_data())
+        f.write(audio.get_wav_data())
 
 
-def speech_to_text():
-    audio = get_audio()
+def speech_to_text(audio):
     recognizer = sr.Recognizer()
-    text= recognizer.recognize_google(audio)
+    text = recognizer.recognize_google(audio)
     return text
 
 
 def get_audio():
     recognizer = sr.Recognizer()
-    for index, name in enumerate(sr.Microphone.list_microphone_names()):
-        print(f"{index}: {name}")
-    with sr.Microphone(device_index=1) as source:
+    device = get_input_device()
+    with sr.Microphone(device_index=device) as source:
         print("Recording ...")
         audio = recognizer.listen(source)
     return audio
 
 
+def get_input_device():
+     mic_devices = sr.Microphone.list_microphone_names()
+     for index, device in enumerate(mic_devices):
+          if "mapper" not in device.lower() and "virtual" not in device.lower():
+               return index
+
+
 if __name__ == "__main__":
-    record_speech()
+    record_speech(get_audio())
