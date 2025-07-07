589e5127f50149201226f7a33951c177d41198a6
jiayizhang19 <j.zhang19@outlook.com>
2025-07-07 09:31:43 +0800

    complete process dialogue and live speech

--- project/project.py
+++ project/project.py
@@ -5,44 +5,82 @@ import numpy as np
 import pyaudio
 import keyboard
 from scipy.io import wavfile
+# from gtts import gTTS
+# import playsound
+import os
 
 def main():
-    mode = input("Live Transcrption, Dialogue Transcription or Recording Transcription: ")
-    if "dialogue" in mode.lower():
-        dialogue_transcipt()
-    elif "live" in mode.lower():
-        live_transcript()
+    task = get_task()
+    text = process_dialogue(task="transcribe")
+    print(">", text)
+    # text = "this is CS50"
+    # text_to_speech(text)
+
+
+
+def get_task():
+    while True:
+        text = input("Press 1 for translate, 2 for transcribe: ")
+        if "1" in text:
+            return "translate"
+        elif "2" in text:
+            return "transcribe"
+        else:
+            continue
+
+
+def get_speech_type(text):
+    ...
+
+
+def text_to_speech(text):
+    ...
+    # tts = gTTS()
 
     
-def live_transcript():
+def process_live_speech(task="transcribe"):
     """
     chunk_size: the number of samples
     sample_rate: the number of samples per second
     chunk_size / sample_rate = xx seconds, e.g. 2048 / 16000 = 0.128s
     """
-    audio, stream, chunk_size = initialize_audio_stream(chunk_size=2048)
+    # Live audio config
+    sample_rate = 16000
+    chunk_size = 2048
+    min_chunk_seconds = 2
+    overlap_duration = 0.5
+    min_samples = sample_rate * min_chunk_seconds   # Buffer 2 seconds of audio
+    overlap_samples = int(sample_rate * overlap_duration)   # 0.5 second of overlap
+
+    audio, stream, chunk_size = initialize_audio_stream(chunk_size=chunk_size)
     audio_buffer = np.array([], dtype=np.int16)
-    min_sample = 16000 * 2  # Buffer 2 seconds of audio
+    prev_tail = np.array([], dtype=np.int16)  
     print("Recording ... Press Enter to stop")
     print("> ", end="")
     try:
         while not keyboard.is_pressed("enter"):
             # Each data is a small NumPy array
             raw_data = stream.read(num_frames=chunk_size, exception_on_overflow=False)
-            audio_chunk = np.frombuffer(raw_data, dtype=np.int16)
-            audio_buffer = np.concatenate((audio_buffer, audio_chunk))
-            # print(f"audio_buffer is {audio_buffer}")
-            if len(audio_buffer) >= min_sample:
-                text = transcript_audio(audio_buffer)
+            new_chunk = np.frombuffer(raw_data, dtype=np.int16)
+            audio_buffer = np.concatenate((audio_buffer,new_chunk))
+            if len(audio_buffer) >= min_samples:
+                chunk_with_tail = np.concatenate((prev_tail, audio_buffer))
+                text = speech_to_text(chunk_with_tail, task)
+                prev_tail = audio_buffer[-overlap_samples:]
                 audio_buffer = np.array([], dtype=np.int16)
                 if text:
-                    print(f"{text}", end=" ", flush=True)
+                    print(f"{text}", end="", flush=True)
     finally:
-        clean_up_audio_stream(stream, audio)
+        if len(audio_buffer) > 0:
+            final_chunk = np.concatenate((prev_tail, audio_buffer))
+            text = speech_to_text(final_chunk, task)
+            if text:
+                print(f"{text}", end=" ", flush=True)
         print("\n> End...")
+        clean_up_audio_stream(stream, audio)
+        
 
-
-def dialogue_transcipt():
+def process_dialogue(task="transcribe"):
     """
     Records audio in chunks until Enter key is pressed.
     Returns Numpy array of recorded audio data.
@@ -54,32 +92,33 @@ def dialogue_transcipt():
         # Each data is a small NumPy array
         data = stream.read(num_frames=chunk_size, exception_on_overflow=False)
         frames.append(np.frombuffer(data, dtype=np.int16))
-    
-    clean_up_audio_stream(stream=stream, audio=audio)
     # combine each small array to one long array for whisper
     audio_data = np.concatenate(frames)
-    return transcript_audio(audio_data)
+    clean_up_audio_stream(stream=stream, audio=audio)
+    return speech_to_text(audio_data, task)
+    
 
-
-def wav_recording_transcript(file):
+def process_wav_recording(file, task="transcribe"):
     """
     Load .wav file and convert to numpy array
     """
     _, audio_data = wavfile.read(file)
     if len(audio_data.shape) > 1:
         audio_data = audio_data.mean(axis=1)
-    return transcript_audio(audio_data)
+    return speech_to_text(audio_data, task)
 
 
-def transcript_audio(audio_data, model_size="base"):
+def speech_to_text(audio_data, task, model_size="base"):
     """
-    Convert audio to text with Whisper
+    Convert audio to text with Whisper, with the default task 
     Arg:
         audio_data: audio data in 16-bit integer type in numpy array, ranging from -32768 to 32727.
-        Need to convert audio data from 16-bit int to 32-bit float ranging from -1.0 to 1.0, expected by most audio processing models.
-        If the audio input is a recorded .wav, below line won't work, as the .wav file is a str type
+                    Need to convert audio data from 16-bit int to 32-bit float ranging from -1.0 to 1.0, expected by most audio processing models.
+                    If the audio input is a recorded .wav, below line won't work, as the .wav file is a str type.
+        task: transcribe or translate
+        model_size: the size of whisper, it could be tiny, base (default value), small, large 
     Returns:
-        transcript
+        text
     """
     audio_float = audio_data.astype(np.float32) / 32768.0
     # =================================================================================== 
@@ -95,9 +134,10 @@ def transcript_audio(audio_data, model_size="base"):
         )
     segments, _ = model.transcribe(
         audio_float, 
-        beam_size=3,
-        vad_filter=True, # aviod wasting whisper's time and CPU on silence
-        language="en"
+        beam_size=5, # better decoding accuracy
+        vad_filter=True, # set to True aviods wasting whisper's time and CPU on silence
+        # language="en",
+        task=task
         )
     return " ".join(segment.text for segment in segments)
     
@@ -129,8 +169,6 @@ def clean_up_audio_stream(stream, audio):
 
 
 if __name__ == "__main__":
-    # main()
-    # print(wav_recording_transcript("manual_recording.wav"))
-    # print(dialogue_transcipt())
-    live_transcript()
+    main()
+
     
