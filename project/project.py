00d5f1ec8db407e1cc609f64773538b5479dc32d
jiayizhang19 <j.zhang19@outlook.com>
2025-07-12 16:42:24 +0800

    update design for final project

--- project/project.py
+++ project/project.py
@@ -1,128 +1,66 @@
 from faster_whisper import WhisperModel
-from zhconv import convert
-import numpy as np
-import pyaudio
-import keyboard
 from googletrans import Translator
-import edge_tts
-import playsound
-import os
-import tempfile
+from datetime import timedelta
 import asyncio
-
-en_to_ch = {"speech": "en", "src": "en", "dest": "zh-CN", "voice": "zh-CN-XiaoxiaoNeural"}
-ch_to_en = {"speech": "zh", "src": "zh-CN", "dest": "en", "voice": "en-US-JennyNeural"}
 
 model = WhisperModel("base", device="cpu", compute_type="int8")
 translator = Translator()
 
-
-def main():
-    choice = get_choice()
-    text = speech_to_text(choice["speech"])
-    print("> Speech: ", text)
-    translation = translate_text(text=text, src=choice["src"], dest=choice["dest"])
-    print("> Translation: ", translation)
-    asyncio.run(text_to_speech(translation, voice=choice["voice"]))
+def main(audio):
+    text = extract_text_from_speech(audio)
+    save_to_txt(text)
+    translation = translate_text(text=text, src="en", dest="zh-CN")
+    print("Translation: ", translation, sep="\n")
 
 
-def get_choice():
-    while True:
-        text = input("Press 1 for en to ch, 2 for ch to en: ")
-        if "1" in text:
-            return en_to_ch
-        elif "2" in text:
-            return ch_to_en
+def extract_text_from_speech(audio):
+    segments, _ = model.transcribe(
+        audio,
+        beam_size=5 # larger number leads to higher accurancy
+    )
+    full_text = []
+    for segment in segments:
+        start = format_timestamp(int(segment.start))
+        text = start + " " + segment.text
+        full_text.append(text)
+    return full_text
+
+
+async def save_to_txt(text, translation=False):
+    with open("transcription.txt", "w", encoding="utf-8") as f:
+        if translation:
+            for t in text:
+                f.write(f"{t}\n")
+                ch = await translate_text(t)
+                f.write(f"{ch}\n")
         else:
-            continue
+            for t in text:
+                f.write(f"{t}\n")
+    print("Saved to transcription.txt")
 
 
-def translate_text(text, src, dest):
-    result = translator.translate(text, src=src, dest=dest)
+async def translate_text(text, src="en", dest="zh-CN"):
+    result = await translator.translate(text, src=src, dest=dest)
     return result.text
 
 
-async def text_to_speech(text, voice):
+def format_timestamp(sec):
     """
-    create a temp .mp3 file, then play it instantly and remote it
+    Format timestamp to minutes and seconds (MM:SS)
+    Arg: second in integer
+    Return: timestamp in 00:00
     """
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp:
-        output_file = temp.name
-    communicate = edge_tts.Communicate(text=text, voice=voice)
-    await communicate.save(output_file)
-    playsound.playsound(output_file)
-    os.remove(output_file)
-
-
-def speech_to_text(language):
-    """
-    Convert audio to text with Whisper, with the default task as transcribe
-    Arg:
-        audio_data: audio data in 16-bit integer type in numpy array, ranging from -32768 to 32727.
-                    Need to convert audio data from 16-bit int to 32-bit float ranging from -1.0 to 1.0, expected by most audio processing models.
-                    If the audio input is a recorded .wav, below line won't work, as the .wav file is a str type.
-        task: transcribe or translate
-        model_size: the size of whisper, it could be tiny, base (default value), small, large 
-    Returns:
-        text
-    """
-    audio_data = process_dialogue_data()
-    audio_float = audio_data.astype(np.float32) / 32768.0
-    segments, _ = model.transcribe(
-        audio_float, 
-        beam_size=5, # better decoding accuracy with the larger number
-        vad_filter=True, # set to True aviods wasting whisper's time and CPU on silence
-        language=language,
-        )
-    text = " ".join(segment.text for segment in segments)
-    return convert(text, "zh-cn") if language == "zh" else text
+    tm = str(timedelta(seconds=sec)).split(".")[0]
+    tm = tm[2:]
+    return tm
     
 
-def process_dialogue_data():
-    """
-    Records audio in chunks until Enter key is pressed.
-    Returns Numpy array of recorded audio data for whisper usage in speech_to_text
-    """
-    audio, stream, chunk_size = initialize_audio_stream()
-    frames = []
-    print("Recording ... Press Enter to stop")
-    while not keyboard.is_pressed("enter"):
-        # Each data is a small NumPy array
-        data = stream.read(num_frames=chunk_size, exception_on_overflow=False)
-        frames.append(np.frombuffer(data, dtype=np.int16))
-    # combine each small array to one long array for whisper audio input
-    audio_data = np.concatenate(frames)
-    clean_up_audio_stream(stream=stream, audio=audio)
-    return audio_data
-
-
-def initialize_audio_stream(sample_rate=16000, chunk_size=1024):
-    """
-    Args:
-        sample_rate: audio sample rate (default 16000)
-        chunk_size: audio frames per buffer 
-    """
-    audio = pyaudio.PyAudio()
-    stream = audio.open(
-        format=pyaudio.paInt16,
-        channels=1,
-        input=True,
-        rate=sample_rate,
-        frames_per_buffer=chunk_size
-    )
-    return audio, stream, chunk_size
-
-
-def clean_up_audio_stream(stream, audio):
-    """
-    Clean up stream and audio to release resources
-    """
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
-
-
 if __name__ == "__main__":
-    main()
+    audio = "03_Whats_for_Dinner.mp4"
+    # main(audio)
+    text = extract_text_from_speech(audio)
+    asyncio.run(save_to_txt(text))
+    # print(asyncio.run(translate_text("How are you", src="en", dest="zh-CN")))
+    # print(format_timestamp(6.84))
 
     
